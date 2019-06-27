d4517fd3000ab71230d251258ea35f444f951eb5
Ulises Santana <ulisesantana@gmail.com>
2019-06-27 21:34:42 +0100

    Updates

---
+++ precache-manifest.8399ad77055ae666e3a5d9be183e2a2a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4973ee2f96389c9a8190e60388f0910",
+    "url": "/bizenter/index.html"
+  },
+  {
+    "revision": "c1ee4aadfa13668d354c",
+    "url": "/bizenter/static/css/2.b398f238.chunk.css"
+  },
+  {
+    "revision": "b3c397e4c66a1a049b87",
+    "url": "/bizenter/static/css/main.fed9eec1.chunk.css"
+  },
+  {
+    "revision": "c1ee4aadfa13668d354c",
+    "url": "/bizenter/static/js/2.17a114c2.chunk.js"
+  },
+  {
+    "revision": "b3c397e4c66a1a049b87",
+    "url": "/bizenter/static/js/main.bd059f1f.chunk.js"
+  },
+  {
+    "revision": "ae124ee2a5f5c533a131",
+    "url": "/bizenter/static/js/runtime~main.27989e42.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/bizenter/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/bizenter/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/bizenter/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/bizenter/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/bizenter/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/bizenter/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/bizenter/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/bizenter/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/bizenter/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/bizenter/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/bizenter/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/bizenter/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/bizenter/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/bizenter/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/bizenter/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/bizenter/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
